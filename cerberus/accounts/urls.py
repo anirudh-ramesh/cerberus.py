c2a739e0bfd5a9127717d2e4008abc2b68e2c330
snehlata-mobcoder <snehlata.sharma@mobcoder.com>
2023-01-13 17:08:49 +0530

    Dummy login added

---
+++ cerberus/accounts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from accounts import views as UserViews
+
+
+urlpatterns = [
+    path('', UserViews.LoginView.as_view(), name="login"),
+]
--- cerberus/accounts/views.py
+++ cerberus/accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django_keycloak.backends import KeycloakAuthenticationBackend as user_auth
+from django.views.generic import View
+from django.http import HttpResponse
 
-# Create your views here.
+
+class LoginView(View):
+
+    def get(self, request):
+        return render(
+            request, 
+            "accounts/login.html", 
+            {},
+            )
+
+    def post(self,request):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        if user_auth.authenticate(username, password):
+            print("Condition passed")
+            return HttpResponse("Hello User")
+        else:
+            return HttpResponse("Not Successful")
