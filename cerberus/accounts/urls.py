cde8279329f26a6ea00163cc1f570dfd7e318af5
snehlata-mobcoder <snehlata.sharma@mobcoder.com>
2023-02-03 11:04:28 +0530

    user views added

--- cerberus/accounts/urls.py
+++ cerberus/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ServerList, UserList
+from .views import ServerList, UserList, UserAccessAPI
 
 
 urlpatterns = [
     path('', ServerList.as_view(), name="server_list"),
     path('user/', UserList.as_view(), name="user_list"),
+    path("token/", UserAccessAPI.as_view(), name="user_access"),
 ]
--- cerberus/accounts/views.py
+++ cerberus/accounts/views.py
@@ -1,6 +1,45 @@
-from django_keycloak.models import Server, OpenIdConnectProfile
-from django.views.generic import ListView
+from django_keycloak.models import Server, Realm, Client
+from django.views.generic import ListView, View
 from django.contrib.auth.models import User
+import requests
+from django.http import HttpResponse
+import json
+from django_keycloak.auth.backends import KeycloakAuthorizationBase as keycloak_auth
+
+
+class UserAccessAPI(View):
+    def get(self, request):
+
+        server = Server.objects.first().url
+
+        realm = Realm.objects.first()
+
+        client = Client.objects.get(realm=realm)
+
+        print(keycloak_auth.get_user())
+
+        user = self.request.user
+        password = self.request.user.password
+
+        print("User = ", user, "Password = ", password)
+
+        user_data = {
+                "username" : "cerberus_user",
+                "password" :"cerberus@123",
+                "client_id":client.client_id,
+                "client_secret": client.secret,
+                "grant_type" : "password",
+            }
+        url = f"{server}auth/realms/{realm}/protocol/openid-connect/token"
+        response = requests.post(
+            url=url,
+            data=user_data,
+        )
+
+        access_token=json.loads(response.text)['access_token']
+
+        print(access_token)
+        return HttpResponse("Successfully created")
 
 
 class ServerList(ListView):
@@ -22,6 +61,3 @@ class UserList(ListView):
         qs = qs.order_by("-id")
         print("Queryset :", qs)
         return qs
-    
-
-    
