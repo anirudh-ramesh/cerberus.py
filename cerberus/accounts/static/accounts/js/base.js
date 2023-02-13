30cec170ac20e2aef85772cab982d090d71d445f
snehlata-mobcoder <snehlata.sharma@mobcoder.com>
2023-02-13 12:36:45 +0530

    Bottom js removed

--- cerberus/accounts/static/accounts/js/base.js
+++ cerberus/accounts/static/accounts/js/base.js
@@ -1,7 +1,14 @@
 function logout(){
-    debugger;
     var access_token = localStorage.getItem("access_token")
     post_data = {"access_token":access_token}
+    $.ajax({
+            type: 'POST',
+            url : "/logout/",
+            success: function(response){
+                console.log(response.messages);
+            },
+        });
+
 
     data = ajaxCall('/logout/', post_data).then((response)=>{
 
