80909477ac19f29f563d6699f11b725e6880bc00
mahendr <mahendra.chaurasiya@mobcoder.com>
2023-02-21 14:48:08 +0530

    solved error in battery_pack_sr_no at update battery

--- cerberus/accounts/static/accounts/js/base.js
+++ cerberus/accounts/static/accounts/js/base.js
@@ -19,9 +19,10 @@ function logout(){
 }
 
 function delete_battery(){
+    console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
     var battery_pack_sr_no = $("#delete_battery").data("model");
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
-    console.log(csrftoken)
+    console.log(csrftoken, 'aksdhvkjasvdkjasd')
     $.ajax({
         type: "POST",
         url: '/deletebattery/',
@@ -53,12 +54,29 @@ function password_show(){
 }
 
 function update_battery(){
-    
-    var battery_pack_sr_no = $(this).data("battery_pack_sr_no")
+    console.log("sssssssssddddd@@@@@@@ mahendra")
+    // var battery_pack_sr_no = $(this).data("battery_pack_sr_no")
+    battery_pack_sr_no = $("#updated_battery_pack_sr_no").data("model");
     var model_name = $(this).data("model_name")
-
-    $("#updated_model_name").val(model_name)
-    $("#updated_battery_pack_sr_no").val(battery_pack_sr_no)
+    console.log("sssssssssddddd battery pack",battery_pack_sr_no)
+    const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+    // $("#updated_model_name").val(model_name)
+    // $("#updated_battery_pack_sr_no").val(battery_pack_sr_no)
+    url='/update_battery/'+battery_pack_sr_no
+    // $.ajax({
+    //     type: "POST",
+    //     url: url,
+    //     headers:{'X-CSRFToken': csrftoken},
+    //     data: {
+            
+    //         "battery_pack_sr_no":battery_pack_sr_no,
+    //     },
+    //     // dataType: "json",
+    //     success: function (data) {  
+    //         alert("successfully Deleted")
+    //     },
+        
+    // });
 }
 
 
@@ -72,3 +90,78 @@ $(document).ready(function(){
     $(document).on("click", "#hide_password", password_hide);
     
 });
+
+// function logout(){
+    
+//     var access_token = localStorage.getItem("access_token")
+//     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+    
+//     $.ajax({
+//         type: "POST",
+//         url: '/logout/',
+//         headers:{'X-CSRFToken': csrftoken},
+//         data: {
+//             "access_token":access_token,
+//         },
+//         dataType: "json",
+//         success: function (data) {  
+//             alert("successfully Logged out")
+//         },
+        
+//     });
+// }
+
+// function delete_battery(){
+//     var battery_pack_sr_no = $("#delete_battery").data("model");
+//     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+//     console.log(csrftoken)
+//     $.ajax({
+//         type: "POST",
+//         url: '/deletebattery/',
+//         headers:{'X-CSRFToken': csrftoken},
+//         data: {
+            
+//             "battery_pack_sr_no":battery_pack_sr_no,
+//         },
+//         dataType: "json",
+//         success: function (data) {  
+//             alert("successfully Deleted")
+//         },
+        
+//     });
+// }
+
+
+// function password_hide(){
+//     $("#password").prop("type", "password");
+//     $("#hide_password").hide();
+//     $("#show_password").show();
+// }
+
+
+// function password_show(){
+//     $("#password").prop("type", "text");
+//     $("#hide_password").show();
+//     $("#show_password").hide();
+// }
+
+// function update_battery(){
+    
+//     var battery_pack_sr_no = $(this).data("battery_pack_sr_no")
+//     var model_name = $(this).data("model_name")
+
+//     $("#updated_model_name").val(model_name)
+//     $("#updated_battery_pack_sr_no").val(battery_pack_sr_no)
+// }
+
+
+// $(document).ready(function(){
+//     $("#hide_password").hide();
+
+//     $(document).on("click", "#logout_btn", logout);
+//     $(document).on("click", "#delete_battery", delete_battery);
+//     $(document).on("click", "#update_battery", update_battery);
+//     $(document).on("click", "#show_password", password_show);
+//     $(document).on("click", "#hide_password", password_hide);
+    
+// });
--- cerberus/accounts/views.py
+++ cerberus/accounts/views.py
@@ -455,7 +455,7 @@ class DeleteBattery(View):
     
 
 class UpdateBattery(View):
-
+    
     def get(self, request, battery_pack_sr_no):
 
         url = "http://iot.igt-ev.com/battery/"
@@ -512,11 +512,11 @@ class UpdateBattery(View):
                     elif key == "Battery Pack Casing":
                         battery_dict["battery_pack_casing"] = value["value"]
 
-
+                print("22222222222222222222",battery_dict)
             return render(request, 'accounts/update_battery.html', battery_dict)
     
     def post(self, request, battery_pack_sr_no):
-
+        # print("22223333311111",request.POST)
         model_name = request.POST.get("model_name", None)
         battery_pack_sr_no = request.POST.get("battery_pack_sr_no", None)
         bms_type = request.POST.get("bms_type", None)
@@ -530,6 +530,8 @@ class UpdateBattery(View):
         battery_cell_type = request.POST.get("battery_cell_type", None)
 
         battery_data = {}
+        # print("<<<<   battery data   >>>>",battery_data,model_name,battery_pack_sr_no,bms_type,warranty_start_date,)
+        # print("<<<<   battery data   >>>>",model_name,battery_pack_sr_no,bms_type,warranty_start_date,status)
         if model_name:
             battery_data["model_name"]= model_name
         if battery_pack_sr_no:
@@ -554,17 +556,134 @@ class UpdateBattery(View):
             battery_data["battery_cell_type"] = str(battery_cell_type)
         
 
-        print(battery_data)
-
+        print(">>>>>>>>>>>>",battery_data)
         headers = {"Content-Type": "application/json; charset=utf-8"}
 
         url = "http://iot.igt-ev.com/battery/battery_pack_sr_no/"+str(battery_pack_sr_no)
 
         response = requests.request("PATCH", url, headers=headers, data=json.dumps(battery_data), json=json.dumps(battery_data))
 
-        print(response.__dict__)
+        print("--->",response)
         
+        print('ssssssssssssssssssssssssssssss')
         if response.status_code in [201, 202, 203, 204, 205, 200]:
             return redirect('get_battery')
-
+      
         return render(request, 'accounts/update_battery.html')
+
+
+
+# class UpdateBattery(View):
+
+#     def get(self, request, battery_pack_sr_no):
+
+#         url = "http://iot.igt-ev.com/battery/"
+
+#         if battery_pack_sr_no:
+    
+#             response = requests.get(
+#                 url = url,
+#                 params={"battery_pack_sr_no":battery_pack_sr_no}
+#             )
+
+#             if response and response.status_code in [200, 201, 202, 203, 204]:
+                
+#                 dict_response = response.__dict__
+
+#                 dict_json_response = json.loads(dict_response["_content"])
+
+#                 battery_dict = {}
+
+#                 battery_dict["battery_pack_sr_no"] = dict_json_response["asset_tag"]
+                
+#                 for key, value in dict_json_response["model"].items():
+#                     if key == "name":
+#                         battery_dict["model_name"] = value
+                
+#                 for key, value in dict_json_response["created_at"].items():
+#                     if key == "formatted":
+#                         battery_dict["warranty_start_date"] = value
+
+#                 battery_dict["warranty_duration"] = dict_json_response["warranty_months"]
+                
+#                 for key ,value in dict_json_response["status_label"].items():
+#                     if key == "name":
+
+#                         battery_dict["status"] = value
+                
+#                 for key, value in dict_json_response["custom_fields"].items():
+                    
+#                     if key == "Battery Cell Chemistry":
+#                         battery_dict["battery_cell_chemistry"] = value["value"]
+                        
+#                     elif key == "Battery Pack Nominal Voltage":
+#                         battery_dict["battery_pack_nominal_voltage"] = value["value"]
+
+#                     elif key == "Battery Pack Nominal Charge Capacity":
+#                         battery_dict["battery_pack_nominal_charge_capacity"] = value["value"]
+                        
+#                     elif key == "BMS Type":
+#                         battery_dict["bms_type"] = value["value"]
+                        
+#                     elif key == "Battery Cell Type":
+#                         battery_dict["battery_cell_type"] = value["value"]
+
+#                     elif key == "Battery Pack Casing":
+#                         battery_dict["battery_pack_casing"] = value["value"]
+
+
+#             return render(request, 'accounts/update_battery.html', battery_dict)
+    
+#     def post(self, request, battery_pack_sr_no):
+#         print("tttttttttttt",request.POST)
+#         model_name = request.POST.get("model_name", None)
+#         battery_pack_sr_no = request.POST.get("battery_pack_sr_no", None)
+#         bms_type = request.POST.get("bms_type", None)
+#         warranty_start_date = request.POST.get("warranty_start_date", None)
+#         warranty_duration = request.POST.get("warranty_duration", None)
+#         status = request.POST.get("status", None)
+#         battery_cell_chemistry = request.POST.get("battery_cell_chemistry", None)
+#         battery_pack_nominal_voltage = request.POST.get("battery_pack_nominal_voltage", None)
+#         battery_pack_nominal_charge_capacity = request.POST.get("battery_pack_nominal_charge_capacity", None)
+#         battery_pack_casing = request.POST.get("battery_pack_casing", None)
+#         battery_cell_type = request.POST.get("battery_cell_type", None)
+
+#         battery_data = {}
+#         if model_name:
+#             battery_data["model_name"]= model_name
+#         if battery_pack_sr_no:
+#             battery_data["battery_pack_sr_no"] =  int(battery_pack_sr_no)
+#         if bms_type:
+#             battery_data["bms_type"] = str(bms_type)
+#         if warranty_start_date:
+#             battery_data["warranty_start_date"] = str(warranty_start_date)
+#         if warranty_duration:
+#             battery_data["warranty_duration"] = int(warranty_duration)
+#         if status:
+#             battery_data["status"] = str(status)
+#         if battery_cell_chemistry:
+#             battery_data["battery_cell_chemistry"] = str(battery_cell_chemistry)
+#         if battery_pack_nominal_voltage:
+#             battery_data["battery_pack_nominal_voltage"] = int(battery_pack_nominal_voltage)
+#         if battery_pack_nominal_charge_capacity:
+#             battery_data["battery_pack_nominal_charge_capacity"] = int(battery_pack_nominal_charge_capacity)
+#         if battery_pack_casing:
+#             battery_data["battery_pack_casing"] = str(battery_pack_casing)
+#         if battery_cell_type:
+#             battery_data["battery_cell_type"] = str(battery_cell_type)
+        
+
+#         print(battery_data)
+
+#         headers = {"Content-Type": "application/json; charset=utf-8"}
+
+#         url = "http://iot.igt-ev.com/battery/battery_pack_sr_no/"+str(battery_pack_sr_no)
+
+#         response = requests.request("PATCH", url, headers=headers, data=json.dumps(battery_data), json=json.dumps(battery_data))
+
+#         print(response.__dict__)
+        
+#         if response.status_code in [201, 202, 203, 204, 205, 200]:
+#             return redirect('get_battery')
+
+#         return render(request, 'accounts/update_battery.html')
