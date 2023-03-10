3b98f2fce3797e34b11676fbe3a9e68da7e12d69
snehlata-mobcoder <snehlata.sharma@mobcoder.com>
2023-03-10 11:55:21 +0530

    vcu_update

--- cerberus/vcu/urls.py
+++ cerberus/vcu/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from vcu.views import ViewAllVCU, AddVCU
+from vcu.views import ViewAllVCU, AddVCU, DeleteVCU, UpdateVCU
 
 
 urlpatterns = [
     path("vcu_list/", ViewAllVCU.as_view(),name="vcu_list"),
     path("add_vcu/", AddVCU.as_view(), name="add_vcu"),
+    path("deletevcu/", DeleteVCU.as_view(), name="deleter_vcu"),
+    path("update_vcu/<str:vcu_name>/", UpdateVCU.as_view(), name="update_vcu"),
 ]
--- cerberus/vcu/views.py
+++ cerberus/vcu/views.py
@@ -104,17 +104,17 @@ class DeleteVCU(View):
         return render(request, "vcu/vcu_details.html")
     
 
-class UpdateBattery(View):
+class UpdateVCU(View):
     
-    def get(self, request, battery_pack_sr_no):
+    def get(self, request, vcu_name):
         
-        url = "http://iot.igt-ev.com/battery/"
+        url = "http://iot.igt-ev.com/vcu/"
 
-        if battery_pack_sr_no:
+        if vcu_name:
     
             response = requests.get(
                 url = url,
-                params={"battery_pack_sr_no":battery_pack_sr_no}
+                params={"vcu_name":vcu_name}
             )
 
             if response and response.status_code in [200, 201, 202, 203, 204]:
@@ -123,94 +123,51 @@ class UpdateBattery(View):
 
                 dict_json_response = json.loads(dict_response["_content"])
 
-                battery_dict = {}
+                vcu_dict = {}
 
-                battery_dict["battery_pack_sr_no"] = dict_json_response["asset_tag"]
+                vcu_dict["vcu_name"] = dict_json_response["asset_tag"]
                 
-                for key, value in dict_json_response["model"].items():
-                    if key == "name":
-                        battery_dict["model_name"] = value
-                
-                for key, value in dict_json_response["created_at"].items():
-                    if key == "formatted":
-                        battery_dict["warranty_start_date"] = value
-
-                battery_dict["warranty_duration"] = dict_json_response["warranty_months"]
-                
-                for key ,value in dict_json_response["status_label"].items():
-                    if key == "name":
-
-                        battery_dict["status"] = value
-                
-                for key, value in dict_json_response["custom_fields"].items():
-                    
-                    if key == "Battery Cell Chemistry":
-                        battery_dict["battery_cell_chemistry"] = value["value"]
-                        
-                    elif key == "Battery Pack Nominal Voltage":
-                        battery_dict["battery_pack_nominal_voltage"] = value["value"]
-
-                    elif key == "Battery Pack Nominal Charge Capacity":
-                        battery_dict["battery_pack_nominal_charge_capacity"] = value["value"]
-                        
-                    elif key == "BMS Type":
-                        battery_dict["bms_type"] = value["value"]
-                        
-                    elif key == "Battery Cell Type":
-                        battery_dict["battery_cell_type"] = value["value"]
-
-                    elif key == "Battery Pack Casing":
-                        battery_dict["battery_pack_casing"] = value["value"]
-
-            return render(request, 'battery_module/update_battery.html', battery_dict)
+            return render(request, 'vcu/update_vcu.html', vcu_dict)
     
-    def post(self, request, battery_pack_sr_no):
-        
-        model_name = request.POST.get("model_name", None)
-        battery_pack_sr_no = request.POST.get("battery_pack_sr_no", None)
-        bms_type = request.POST.get("bms_type", None)
-        warranty_start_date = request.POST.get("warranty_start_date", None)
-        warranty_duration = request.POST.get("warranty_duration", None)
-        status = request.POST.get("status", None)
-        battery_cell_chemistry = request.POST.get("battery_cell_chemistry", None)
-        battery_pack_nominal_voltage = request.POST.get("battery_pack_nominal_voltage", None)
-        battery_pack_nominal_charge_capacity = request.POST.get("battery_pack_nominal_charge_capacity", None)
-        battery_pack_casing = request.POST.get("battery_pack_casing", None)
-        battery_cell_type = request.POST.get("battery_cell_type", None)
+    def post(self, request, vcu_name):
 
-        battery_data = {}
+
+        vcu_name = request.POST.get("vcu_name")
+        imei = request.POST.get("imei")
+        warranty_start_date =  request.POST.get("warranty_start_date")
+        warranty_duration = request.POST.get("warranty_duration")
+        status = request.POST.get("status")
+        vehicle_type = request.POST.get("vehicle_type")
+        vehicle_configuration = request.POST.get("vehicle_configuration")
+        owner = request.POST.get("owner")
         
-        if model_name:
-            battery_data["model_name"]= model_name
-        if battery_pack_sr_no:
-            battery_data["battery_pack_sr_no"] =  str(battery_pack_sr_no)
-        if bms_type:
-            battery_data["bms_type"] = str(bms_type)
+        vcu_data = {}
+        
+        if vcu_name:
+            vcu_data["vcu_name"]= vcu_name
+        if imei:
+            vcu_data["imei"] =  str(imei)
+        if vehicle_type:
+            vcu_data["bms_type"] = str(vehicle_type)
         if warranty_start_date:
-            battery_data["warranty_start_date"] = str(warranty_start_date)
+            vcu_data["warranty_start_date"] = str(warranty_start_date)
         if warranty_duration:
-            battery_data["warranty_duration"] = int(warranty_duration)
+            vcu_data["warranty_duration"] = int(warranty_duration)
         if status:
-            battery_data["status"] = str(status)
-        if battery_cell_chemistry:
-            battery_data["battery_cell_chemistry"] = str(battery_cell_chemistry)
-        if battery_pack_nominal_voltage:
-            battery_data["battery_pack_nominal_voltage"] = int(battery_pack_nominal_voltage)
-        if battery_pack_nominal_charge_capacity:
-            battery_data["battery_pack_nominal_charge_capacity"] = int(battery_pack_nominal_charge_capacity)
-        if battery_pack_casing:
-            battery_data["battery_pack_casing"] = str(battery_pack_casing)
-        if battery_cell_type:
-            battery_data["battery_cell_type"] = str(battery_cell_type)
+            vcu_data["status"] = str(status)
+        if vehicle_configuration:
+            vcu_data["vehicle_configuration"] = str(vehicle_configuration)
+        if owner:
+            vcu_data["owner"] = str(owner)
         
         headers = {"Content-Type": "application/json; charset=utf-8"}
 
-        url = "http://iot.igt-ev.com/battery/battery_pack_sr_no/"+str(battery_pack_sr_no)
+        url = "http://iot.igt-ev.com/battery/battery_pack_sr_no/"+str(vcu_name)
 
-        response = requests.request("PATCH", url, headers=headers, data=json.dumps(battery_data), json=json.dumps(battery_data))
+        response = requests.request("PATCH", url, headers=headers, data=json.dumps(vcu_data), json=json.dumps(vcu_data))
 
         if response.status_code in [201, 202, 203, 204, 205, 200]:
-            return redirect('viewallbattery')
+            return redirect('vcu_list')
         
-        return render(request, 'battery_module/update_battery.html')
+        return render(request, 'vcu/update_vcu.html')
 
